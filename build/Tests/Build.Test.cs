3b58aea112dff5ee497be6f8184b686f1d64f3be
Vespion <21206333+Vespion@users.noreply.github.com>
2023-03-27 16:46:57 -0700

    ci: fix crash caused when number of test projects is less than 2

--- build/Tests/Build.Test.cs
+++ build/Tests/Build.Test.cs
@@ -51,41 +51,68 @@ partial class Build
 				.Select(Solution.GetProject)
 				.ToArray();
 
-			DotNetTest(c => c
-				.EnableNoBuild()
-				.SetBlameHangTimeout($"{TestTimeout}m")
-				.EnableCollectCoverage()
-				.SetCoverletOutputFormat(CoverletOutputFormat.json)
-				.AddLoggers("xunit")
-				.SetProperty("Exclude", "[xunit.*]*")
-				.SetProperty("SkipAutoProps", "true")
-				.SetProperty("DeterministicReport", "true")
-				.SetProperty("Threshold", CoverageThreshold)
-				.CombineWith(testProjects[..^1], (_, p) => _
-					.SetProjectFile(p)
-					.SetResultsDirectory(TestResultsDirectory / p.Name)
-					.SetCoverletOutput(TestResultsDirectory / "coverage.json")
+			if (testProjects.Length == 0)
+			{
+				//No tests to run
+				return;
+			}
+
+			if (testProjects.Length > 2)
+			{
+				DotNetTest(c => c
+						.EnableNoBuild()
+						.SetBlameHangTimeout($"{TestTimeout}m")
+						.EnableCollectCoverage()
+						.SetCoverletOutputFormat(CoverletOutputFormat.json)
+						.AddLoggers("xunit")
+						.SetProperty("Exclude", "[xunit.*]*")
+						.SetProperty("SkipAutoProps", "true")
+						.SetProperty("DeterministicReport", "true")
+						.SetProperty("Threshold", CoverageThreshold)
+						.CombineWith(testProjects[..^1], (_, p) => _
+							.SetProjectFile(p)
+							.SetResultsDirectory(TestResultsDirectory / p.Name)
+							.SetCoverletOutput(TestResultsDirectory / "coverage.json")
+							.SetProperty("MergeWith", TestResultsDirectory / "coverage.json")
+						), 1, //This cannot run in parallel because it would overwrite the coverage file
+					true
+				);
+
+				DotNetTest(c => c
+					.EnableNoBuild()
+					.SetBlameHangTimeout($"{TestTimeout}m")
+					.EnableCollectCoverage()
+					.SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+					.AddLoggers("xunit")
+					.SetProperty("Exclude", "[xunit.*]*")
+					.SetProperty("SkipAutoProps", "true")
+					.SetProperty("DeterministicReport", "true")
+					.SetProperty("Threshold", CoverageThreshold)
+					.SetProjectFile(testProjects[^1])
+					.SetResultsDirectory(TestResultsDirectory / testProjects[^1].Name)
+					.SetCoverletOutput(TestResultsDirectory / "coverage.xml")
 					.SetProperty("MergeWith", TestResultsDirectory / "coverage.json")
-				), 1, //This cannot run in parallel because it would overwrite the coverage file
-				true
-			);
-			
-			DotNetTest(c => c
-				.EnableNoBuild()
-				.SetBlameHangTimeout($"{TestTimeout}m")
-				.EnableCollectCoverage()
-				.SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
-				.AddLoggers("xunit")
-				.SetProperty("Exclude", "[xunit.*]*")
-				.SetProperty("SkipAutoProps", "true")
-				.SetProperty("DeterministicReport", "true")
-				.SetProperty("Threshold", CoverageThreshold)
-				.SetProjectFile(testProjects[^1])
-				.SetResultsDirectory(TestResultsDirectory / testProjects[^1].Name)
-				.SetCoverletOutput(TestResultsDirectory / "coverage.xml")
-				.SetProperty("MergeWith", TestResultsDirectory / "coverage.json")
-			);
-			
+				);
+			}
+			else
+			{
+				DotNetTest(c => c
+					.EnableNoBuild()
+					.SetBlameHangTimeout($"{TestTimeout}m")
+					.EnableCollectCoverage()
+					.SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+					.AddLoggers("xunit")
+					.SetProperty("Exclude", "[xunit.*]*")
+					.SetProperty("SkipAutoProps", "true")
+					.SetProperty("DeterministicReport", "true")
+					.SetProperty("Threshold", CoverageThreshold)
+					.SetProjectFile(testProjects[0])
+					.SetResultsDirectory(TestResultsDirectory / testProjects[^1].Name)
+					.SetCoverletOutput(TestResultsDirectory / "coverage.xml")
+					.SetProperty("MergeWith", TestResultsDirectory / "coverage.json")
+				);
+			}
+
 			foreach (var testProject in testProjects)
 			{
 				var resultsDirectory = TestResultsDirectory / testProject.Name;
